f2cc7863204bebedb967b89b8ebd17efd64df598
jordan9185 <jordan5011@yahoo.com>
2017-09-11 10:01:55 +0800

    Created Landing View Controller.

--- Patissier-iOS-Objective-C/LandingViewController/LandingViewController.h
+++ Patissier-iOS-Objective-C/LandingViewController/LandingViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  UIViewController+LandingViewController.h
 //  Patissier-iOS-Objective-C
 //
 //  Created by JordanLin on 2017/9/11.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LandingViewController: UIViewController
 
 @end
-
