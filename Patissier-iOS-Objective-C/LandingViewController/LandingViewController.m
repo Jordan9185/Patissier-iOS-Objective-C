fefd9c01ee3186562f3fb4dae7d184a319fb1248
francis830227 <francis830227@gmail.com>
2017-09-11 16:38:06 +0800

    landingViewController update

--- Patissier-iOS-Objective-C/LandingViewController/LandingViewController.m
+++ Patissier-iOS-Objective-C/LandingViewController/LandingViewController.m
@@ -1,5 +1,5 @@
 //
-//  UIViewController+LandingViewController.m
+//  LandingViewController.m
 //  Patissier-iOS-Objective-C
 //
 //  Created by JordanLin on 2017/9/11.
@@ -12,19 +12,26 @@
 
 @end
 
-@interface NYTSection: NSObject
-
-@property (nonatomic) NSString *headline;
-
-@end
-
 @implementation LandingViewController
 
-- (void) viewDidLoad {
+- (void)viewDidLoad {
     [super viewDidLoad];
-    
     NSLog(@"fuck");
 }
 
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
