c8b9faa7fa521cadda76a5fc71f8217a57e10124
jamesKe <iamcdxn@gmail.com>
2017-09-11 14:30:14 +0800

    Add Profile Info Cell

---
+++ Patissier-iOS-Objective-C/Profile/ProfileInfomationTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ProfileInfomationTableViewCell.m
+//  Patissier-iOS-Objective-C
+//
+//  Created by CdxN on 2017/9/11.
+//  Copyright © 2017年 JordanLin. All rights reserved.
+//
+
+#import "ProfileInfomationTableViewCell.h"
+
+@implementation ProfileInfomationTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
