1a3fe7e0c7d66c843ca6853eeaa17162d3c8ab90
jordan9185 <jordan5011@yahoo.com>
2017-09-11 20:55:49 +0800

    Fine tune landing page UI.

--- Patissier-iOS-Objective-C/LandingViewController.m
+++ Patissier-iOS-Objective-C/LandingViewController.m
@@ -10,8 +10,6 @@
 
 @interface LandingViewController ()
 
-@property (strong, nonatomic) CAGradientLayer *backgroundGradientLayer;
-
 @end
 
 @implementation LandingViewController
@@ -28,12 +26,34 @@
     
     self.landingImageView.image = [UIImage imageNamed:@"image-landing.png"];
     
-    //self.landingImageView.contentMode = [UIViewContentModeScaleAspectFill];
+    self.landingImageView.contentMode = UIViewContentModeScaleAspectFill;
 }
 
 - (void)setUpBackgroundGradientView {
     
+    self.GradientView.alpha = 0.85;
     
+    UIColor *colorOne = [UIColor colorWithRed: 3.0 / 255.0
+                                        green: 63.0 / 255.0
+                                         blue: 122.0 / 255.0
+                                        alpha: 1.0];
+    
+    UIColor *colorTwo = [UIColor colorWithRed: 4.0 / 255.0
+                                        green: 107.0 / 255.0
+                                         blue: 149.0 / 255.0
+                                        alpha: 1.0];
+    
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    
+    gradientLayer.colors = [NSArray arrayWithObjects:(id) colorOne.CGColor, colorTwo.CGColor, nil];
+    
+    gradientLayer.frame = self.GradientView.bounds;
+    
+    gradientLayer.startPoint = CGPointMake(0.0, 0.5);
+    
+    gradientLayer.endPoint = CGPointMake(1.0, 0.5);
+    
+    [self.GradientView.layer insertSublayer: gradientLayer atIndex: 0];
     
 }
 
