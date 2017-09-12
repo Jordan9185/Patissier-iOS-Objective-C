682bddeb9efd8c706e9caa0145928b7109c70481
francis830227 <francis830227@gmail.com>
2017-09-12 12:59:18 +0800

    stuck in computed property

---
+++ Patissier-iOS-Objective-C/Profile/Product.h
@@ -0,0 +1,28 @@
+//
+//  Product.h
+//  Patissier-iOS-Objective-C
+//
+//  Created by Francis Tseng on 2017/9/12.
+//  Copyright © 2017年 JordanLin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Product : NSObject
+
+@property NSString *identifier;
+
+@property NSString *name;
+
+@property NSString *price;
+
+//- (NSString*) imageURL {
+//    
+//    NSString *urlString = @"http://52.198.40.72/patissier/products//preview.jpg";
+//    
+//    return @"";
+//}
+
+
+
+@end
---
+++ Patissier-iOS-Objective-C/Profile/Product.m
@@ -0,0 +1,13 @@
+//
+//  Product.m
+//  Patissier-iOS-Objective-C
+//
+//  Created by Francis Tseng on 2017/9/12.
+//  Copyright © 2017年 JordanLin. All rights reserved.
+//
+
+#import "Product.h"
+
+@implementation Product
+
+@end
